624a5b19dafb481587e1097f9e3ad739ca5712f9
Htoo <htooeainlwin12@gmail.com>
2025-01-16 19:11:45 +0700

    [feature] Update json with types

--- lib/noticed/bulk_delivery_methods/confluent_rest_api.rb
+++ lib/noticed/bulk_delivery_methods/confluent_rest_api.rb
@@ -7,7 +7,16 @@ module Noticed
         url = evaluate_option(:url)
         message = evaluate_option(:message)
 
-        json = { records: { key: message[:key], value: message[:value] } }
+        json = {
+          key: {
+            type: 'STRING',
+            data: message[:key]
+          },
+          value: {
+            type: 'JSON',
+            data: message[:value]
+          }
+        }
 
         post_request url, headers: evaluate_option(:headers), json: json
       end
