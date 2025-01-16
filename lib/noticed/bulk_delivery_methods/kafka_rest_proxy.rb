7bb3167e320b50791830877c9e2356c394941fcf
Htoo <htooeainlwin12@gmail.com>
2025-01-16 19:04:05 +0700

    [feature] Add confulent rest api

--- lib/noticed/bulk_delivery_methods/kafka_rest_proxy.rb
+++ lib/noticed/bulk_delivery_methods/kafka_rest_proxy.rb
@@ -1,13 +1,13 @@
 module Noticed
   module BulkDeliveryMethods
-    class KafkaRest < BulkDeliveryMethod
+    class KafkaRestProxy < BulkDeliveryMethod
       required_options :url, :headers, :message
 
       def deliver
         url = evaluate_option(:url)
         message = evaluate_option(:message)
 
-        json = { records: { key: message[:key], value: message[:value] } }
+        json = { records: [{ key: message[:key], value: message[:value] }] }
 
         post_request url, headers: evaluate_option(:headers), json: json
       end
