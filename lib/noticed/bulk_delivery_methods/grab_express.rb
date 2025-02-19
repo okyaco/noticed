5aba704ca76590bee1f2a349332dcfbe4d913839
Breena T <breena@nimblehq.co>
2025-02-19 11:56:45 +0700

    Format comment to one line

--- lib/noticed/bulk_delivery_methods/grab_express.rb
+++ lib/noticed/bulk_delivery_methods/grab_express.rb
@@ -7,8 +7,7 @@ module Noticed
       before_deliver :before_deliver
 
       after_deliver do
-        # Call the lambda explicitly as evaluate_option prematurely calls the lambda
-        # before the response is ready
+        # Call the lambda explicitly as evaluate_option prematurely calls the lambda before the response is ready
         @after_deliver.call(@response)
       end
 
