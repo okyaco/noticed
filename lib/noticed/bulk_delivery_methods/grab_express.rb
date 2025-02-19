5e86d6b5e7b9aca73fc868b3dfd1b1d403febbba
Breena T <breena@nimblehq.co>
2025-02-19 11:46:43 +0700

    Implement before_deliver for Grab Express Bulk delivery

--- lib/noticed/bulk_delivery_methods/grab_express.rb
+++ lib/noticed/bulk_delivery_methods/grab_express.rb
@@ -1,18 +1,22 @@
 module Noticed
   module BulkDeliveryMethods
     class GrabExpress < BulkDeliveryMethod
-      required_options :url, :headers, :json, :after_deliver
-      attr_accessor :request_body, :response, :after_deliver
+      required_options :url, :headers, :json, :before_deliver, :after_deliver
+      attr_accessor :response, :before_deliver, :after_deliver
+
+      before_deliver :before_deliver
 
       after_deliver do
+        # Call the lambda explicitly as evaluate_option prematurely calls the lambda
+        # before the response is ready
         @after_deliver.call(@response)
       end
 
       def deliver
-        @request_body = evaluate_option(:json)
+        @before_deliver = evaluate_option(:before_deliver)
         @after_deliver = evaluate_option(:after_deliver)
 
-        @response = post_request evaluate_option(:url), headers: evaluate_option(:headers), json: @request_body
+        @response = post_request evaluate_option(:url), headers: evaluate_option(:headers), json: evaluate_option(:json)
       end
     end
   end
