97e8a876715241a83d17c83965e0a6e69f488b72
Breena T <breena@nimblehq.co>
2025-02-24 16:49:21 +0700

    Add DELETE HTTP method for API client lib as Grab Express uses DELETE http method to cancel deliveries

--- lib/noticed/bulk_delivery_methods/grab_express.rb
+++ lib/noticed/bulk_delivery_methods/grab_express.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Noticed
   module BulkDeliveryMethods
     class GrabExpress < BulkDeliveryMethod
-      required_options :url, :headers, :json, :before_deliver
+      required_options :url, :headers, :before_deliver
       attr_accessor :response, :before_deliver
 
       before_deliver :before_deliver
@@ -15,7 +17,16 @@ module Noticed
         @before_deliver = evaluate_option(:before_deliver)
         @after_deliver = evaluate_option(:after_deliver)
 
-        @response = post_request evaluate_option(:url), headers: evaluate_option(:headers), json: evaluate_option(:json)
+        # Grab uses POST HTTP method to create deliveries, and DELETE HTTP method to cancel the deliveries
+        method = evaluate_option(:method)&.to_s&.upcase || 'POST'
+
+        @response = case method
+                    when 'DELETE'
+                      delete_request evaluate_option(:url), headers: evaluate_option(:headers)
+                    when 'POST'
+                      post_request evaluate_option(:url), headers: evaluate_option(:headers),
+                                                          json: evaluate_option(:json)
+                    end
       end
     end
   end
