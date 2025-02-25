b3afca569f01e4ff58b939863e95fa3b441cac62
Khanh Tran <khanh.t@nimblehq.co>
2025-02-25 14:39:46 +0700

    [feat] Add Pandago bulk delivery methods

---
+++ lib/noticed/bulk_delivery_methods/pandago.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Noticed
+  module BulkDeliveryMethods
+    class Pandago < BulkDeliveryMethod
+      required_options :url, :headers, :before_deliver
+      attr_accessor :response, :before_deliver
+
+      before_deliver :before_deliver
+
+      after_deliver do
+        # Call the lambda explicitly as evaluate_option prematurely calls the lambda before the response is ready
+        @after_deliver.call(@response) if @after_deliver.present?
+      end
+
+      def deliver
+        @before_deliver = evaluate_option(:before_deliver)
+        @after_deliver = evaluate_option(:after_deliver)
+
+        # Pandago uses POST HTTP method to create deliveries, and DELETE HTTP method to cancel the deliveries
+        method = evaluate_option(:method)&.to_s&.upcase || 'POST'
+
+        @response = case method
+                    when 'DELETE'
+                      delete_request evaluate_option(:url), headers: evaluate_option(:headers)
+                    when 'POST'
+                      post_request evaluate_option(:url), headers: evaluate_option(:headers),
+                                                          json: evaluate_option(:json)
+                    end
+      end
+    end
+  end
+end
