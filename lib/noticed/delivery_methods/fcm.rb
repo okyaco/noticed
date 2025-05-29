1941473a9ba973ee149adf0141129c0871427877
Minh Pham <minh@nimblehq.co>
2025-05-29 10:11:14 +0700

    Add missing header content type

--- lib/noticed/delivery_methods/fcm.rb
+++ lib/noticed/delivery_methods/fcm.rb
@@ -12,9 +12,10 @@ module Noticed
       end
 
       def send_notification(device_token)
+        payload = format_notification(device_token)
         post_request("https://fcm.googleapis.com/v1/projects/#{credentials[:project_id]}/messages:send",
-          headers: {authorization: "Bearer #{access_token}"},
-          json: format_notification(device_token))
+          headers: {authorization: "Bearer #{access_token}", "Content-Type": "application/json"},
+          json: payload)
       rescue Noticed::ResponseUnsuccessful => exception
         if exception.response.code == "404" && config[:invalid_token]
           notification.instance_exec(device_token, &config[:invalid_token])
