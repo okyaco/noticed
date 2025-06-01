86f9474dec2b1a0415bcad23fd0a1e313d16c6ff
Jay Ang <zijie@nimblehq.co>
2025-06-01 16:40:59 +0700

    [OK-398][feature] Add AwsIot bulk delivery method

---
+++ lib/noticed/bulk_delivery_methods/aws_iot.rb
@@ -0,0 +1,34 @@
+require 'aws-sdk-iotdataplane'
+
+module Noticed
+  module BulkDeliveryMethods
+    class AwsIot < BulkDeliveryMethod
+      required_options :url, :credentials, :message
+
+      def deliver
+        endpoint = evaluate_option(:url)
+        credentials = evaluate_option(:credentials).with_indifferent_access
+        message  = evaluate_option(:message)
+
+        client = Aws::IoTDataPlane::Client.new(
+          endpoint: endpoint,
+          region: credentials[:region],
+          credentials: Aws::Credentials.new(credentials[:access_key_id], credentials[:secret_access_key])
+        )
+
+        client.publish(
+          topic: message.fetch(:topic),
+          payload: serialise_payload(message.fetch(:payload)),
+          qos: message.fetch(:qos, 0),
+          retain: message.fetch(:retain, false)
+        )
+      end
+
+      private
+
+      def serialise_payload(payload)
+        payload.is_a?(String) ? payload : payload.to_json
+      end
+    end
+  end
+end
